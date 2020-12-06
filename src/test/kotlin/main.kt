d086285380f1a402494010d452894305df09eea8
Alex Popov <apopov974@gmail.com>
2020-12-06 15:05:22 +0500

    logic for *11 numbers added

--- src/test/kotlin/main.kt
+++ src/test/kotlin/main.kt
@@ -1,7 +1,9 @@
 fun main() {
-    val likes = 0
+    val likes = 2352
     if (likes == 0) {
         println("Никому не понравилось")
+    } else if (likes % 100 == 11 || likes == 11){
+        println("Понравилось $likes людям")
     } else if (likes % 10 == 1) {
         println("Понравилось $likes человеку")
     } else {
